4e0b94a322e86393cf423efbedfeefd6d365c018
salma97 <1500667@eng.asu.edu.eg>
2019-04-25 19:47:24 +0200

    Uploading blank h and c files needed for the whole project

---
+++ std_types.h
@@ -0,0 +1,13 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+#include <stdio.h>
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+typedef unsigned long long uint64_t;
+typedef float float32_t;
+typedef double float64_t;
+
+#endif
