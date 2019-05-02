357b2d2c8af154b5a9bc846375ad59286dedff19
Mokashaa <mohamedokasha125@gmail.com>
2019-05-02 17:19:27 +0200

    fixing bug in ADC_Init function #2

--- Tivac1/adc.c
+++ Tivac1/adc.c
@@ -6,7 +6,12 @@ void ADC0_SS3_Init (void)
 	SYSCTL_RCGCADC_R |= (1<<0);			//enable ADC0 clock
 	ADC0_ACTSS_R &= ~(1<<SS_num);				//disable sample sequncer during configuration
 	ADC0_EMUX_R = (ADC0_EMUX_R& ~(0xF<<SS_num*4)) | (trigger_select<<SS_num*4) ; //choosing the triggering method
-	ADC0_SSMUX3_R = 0x0;
+	ADC0_SSMUX3_R = 0x0;					//input from channel 0 i.e PE3
 	ADC0_SSCTL3_R |= ( (ADC0_SSCTL3_TS0<<3) | (ADC0_SSCTL3_IE0<<2) | (ADC0_SSCTL3_END0<<1));  //configuring sample sequncer 3 control port
+	if(ADC0_SSCTL3_IE0	==	0x1)
+	{
+		ADC0_IM_R |= (1<<3);
+		NVIC_EN0_R |= (1<<17);
+	}
 	ADC0_ACTSS_R |= (1<<SS_num);				//enable sample sequncer during configuration
 }
--- Tivac1/main.c
+++ Tivac1/main.c
@@ -1,10 +1,10 @@
 #include "adc.h"
 #include "uart.h"
 #include "led.h"
-#include "motot.h"
+#include "motor.h"
 #include "temperature_sensor.h"
 
-ADC0SS3_Handler
+void  ADC0SS3_Handler (void)
 {
 	uint8_t temperature = 147.5 - ((247.5*ADC0_SS3_readValue())/4096); /* Convert the ADC value into celcus */ 
 	UART0_send(temperature); /* send the temperature in celcus through uart */
