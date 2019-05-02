a39ee1cd159c5ffa64c38460978640b9da5f2b87
Esraa1Moshsen <rokamohsen403@gmail.com>
2019-05-02 20:21:47 +0200

    Update LCD_writeChar.c

--- Tivac2/LCD_writeChar.c
+++ Tivac2/LCD_writeChar.c
@@ -1,15 +1,8 @@
-#include "tm4c123gh6pm.h"
-
+#include "lcd.h"
  
  void LCD_WriteChar (uint8 LCD_DATA)
 {
-	uint8 RS;
-	uint8 RW;
-	uint8 E;
-	uint8 LCD_DATA;
-	RS=0x02;
-	RW=0x04;
-	E=0x08;
+
 	DIO_WritePort( PORTA, RS, STD_HIGH);
 	DIO_WritePort( PORTA, RW, STD_LOW);
 	DIO_WritePort( PORTA, E, STD_HIGH);
