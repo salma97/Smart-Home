165cd4e77ae3be6978378818f48eccc2e409b653
ganzourii <ganzouri97@gmail.com>
2019-05-02 16:11:16 +0200

    Add the ADC ISR function
    Interrupt function is getting the ADC value of the temp. sensor, then converting the value into celcus and send the value to the second TivaC through UART0. Finally, Acknoweldge to clear the Interrupt flag and start conversion again.

--- Tivac1/main.c
+++ Tivac1/main.c
@@ -1,3 +1,17 @@
+#include "adc.h"
+#include "uart.h"
+#include "led.h"
+#include "motot.h"
+#include "temperature_sensor.h"
+
+ADC0SS3_Handler
+{
+	uint8_t temperature = 147.5 - ((247.5*ADC0_SS3_readValue())/4096); /* Convert the ADC value into celcus */ 
+	UART0_send(temperature); /* send the temperature in celcus through uart */
+	ADC0_SS3_ACK();	  /* acknoweldge the adc interrupt to clear the flag in RIS register and restart the conversion of the temp. */
+	
+}
+
 void SystemInit()
 {
 }
