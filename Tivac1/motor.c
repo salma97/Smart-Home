24d977879e02bd4e477ff68df6aa419449fa5565
omarnagah <omarnagah4@gmail.com>
2019-05-03 23:09:24 +0200

    updating anticlockwise function, adding global var

--- Tivac1/motor.c
+++ Tivac1/motor.c
@@ -1,30 +1,18 @@
 #include "motor.h"
-
+const char steps[ ] = {0x9, 0x3, 0x6, 0xC};
+int cnt =8;
 void Motor_Clockwise (void)
 {
 	
 }
 
 void Motor_AntiClockwise (void)
-{
-	//assume motor steps = 200/revolution
-	// 1.8 degree /step // angle = 4 loops*4 steps* 1.8 = 28.8
- for (int i=0;i<4;i++)
+{// assume step angle =5 degree
+	// (30 degree/ angle for one step)
+ for (int i=0;i<6;i++)
   {
    GPIO_PORTB_DATA_R &= ~ 0xF;
-   GPIO_PORTB_DATA_R |= 0x0C;
-   delay(10);
-
-   GPIO_PORTB_DATA_R &= ~ 0xF;
-   GPIO_PORTB_DATA_R |= 0x06;
-   delay(10);
-
-   GPIO_PORTB_DATA_R &= ~ 0xF;
-   GPIO_PORTB_DATA_R |= 0x03;
-   delay(10);
-
-   GPIO_PORTB_DATA_R &= ~ 0xF;
-   GPIO_PORTB_DATA_R |= 0x09;
+   GPIO_PORTB_DATA_R |= steps[cnt-- & 3];
    delay(10);
   }
 }
