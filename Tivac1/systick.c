d194112081ce2a7f276155862ad1caa0a7ef933e
Esraa1Moshsen <rokamohsen403@gmail.com>
2019-05-03 15:03:33 +0200

    Systick_Init  #22

--- Tivac1/systick.c
+++ Tivac1/systick.c
@@ -3,6 +3,10 @@
 #include "std_types.h"
 
 void systick_init(void){
+		NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
+		NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M;
+		NVIC_ST_CURRENT_R = NVIC_ST_CURRENT_S;
+		NVIC_ST_CTRL_R |= (NVIC_ST_CTRL_CLK_SRC | NVIC_ST_CTRL_ENABLE);
 	
 }
 
