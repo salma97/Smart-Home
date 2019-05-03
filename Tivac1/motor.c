0dc68887158f9362ac89d9a1bb506913184ad61a
omarnagah <omarnagah4@gmail.com>
2019-05-03 17:40:48 +0200

    motor anticlock wise function

--- Tivac1/motor.c
+++ Tivac1/motor.c
@@ -7,5 +7,24 @@ void Motor_Clockwise (void)
 
 void Motor_AntiClockwise (void)
 {
-	
+	//assume motor steps = 200/revolution
+	// 1.8 degree /step // angle = 4 loops*4 steps* 1.8 = 28.8
+ for (int i=0;i<4;i++)
+  {
+   GPIO_PORTB_DATA_R &= ~ 0xF;
+   GPIO_PORTB_DATA_R |= 0x0C;
+   delay(10);
+
+   GPIO_PORTB_DATA_R &= ~ 0xF;
+   GPIO_PORTB_DATA_R |= 0x06;
+   delay(10);
+
+   GPIO_PORTB_DATA_R &= ~ 0xF;
+   GPIO_PORTB_DATA_R |= 0x03;
+   delay(10);
+
+   GPIO_PORTB_DATA_R &= ~ 0xF;
+   GPIO_PORTB_DATA_R |= 0x09;
+   delay(10);
+  }
 }
