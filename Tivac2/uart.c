3bae9f72bcdff136b680ee80e7e870fd35cee5c7
mariamelsaayed <mariamelsaayed.me@gmail.com>
2019-05-04 00:42:23 +0200

    UART files updated with all funcs

--- Tivac2/uart.c
+++ Tivac2/uart.c
@@ -17,9 +17,32 @@ void UART0_Init(void)
 	
 }
 
+void UART0_Handler(void)
+{
+	uint8_t word = 0;
+	word = UART0_Read();
+	if (word & (1 << 7))
+	{
+		LED_Intensity(word & ~(1 << 7));
+	}
+	else if (word & (1 << 0))
+	{
+		Motor_Clockwise();
+	}
+	else if (word & (1 << 4))
+	{
+		Motor_AntiClockwise();
+	}
+}
+
 uint8_t UART0_Read (void)
 {
  while((UART0_FR_R&0x0010)!=0);
  return(UART0_DR_R&0xFF);
 
+}
+
+void UART0_Write(uint8_t data) {
+	while ((UART0_FR_R&UART_FR_TXFF) != 0);
+	UART0_DR_R = data;
 }
