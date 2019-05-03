26408359a64251b4c3d9cec9830de9eac2df4279
Ahmed Anwar <36543394+AhmedAnwar97@users.noreply.github.com>
2019-05-03 14:40:25 +0200

    Update systick.c

--- Tivac1/systick.c
+++ Tivac1/systick.c
@@ -7,3 +7,9 @@ void systick_wait1ms(void){
 	NVIC_ST_CURRENT_R = 0;
 	while((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT) != NVIC_ST_CTRL_COUNT);
 }
+
+void delay(uint16_t i){
+	for(uint16_t j=0; j<i; j++){
+		systick_wait1ms();
+	}
+}
