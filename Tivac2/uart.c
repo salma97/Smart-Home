72a71f8c2004e9c57518c3b69379f93a4cc06994
HaidySamir1696 <haidysameer@hotmail.com>
2019-05-03 14:47:16 +0200

    UART0_Read  #2

--- Tivac2/uart.c
+++ Tivac2/uart.c
@@ -11,8 +11,14 @@ void UART0_Init(void)
 	UART0_FBRD_R = fBRD;
 	DIO_WritePort(UART0_LCRH_R, UART_CTLINE_M, STD_HIGH);//8 bits word, even parity bit and 1 stop bit enabled
 	DIO_WritePort(UART0_CTL_R, UART_CTL_M_EN, STD_HIGH); //enable TX, RX and UART
-	DIO_WritePort(GPIO_PORTA_AFSEL_R, GPIO_MASK, STD_HIGH); 
+	DIO_WritePort(GPIO_PORTA_AFSEL_R, GPIO_MASK, STD_HIGH);
 	DIO_WritePort(GPIO_PORTA_DEN_R, GPIO_MASK, STD_HIGH);
 	DIO_WritePort(GPIO_PORTA_AMSEL_R, GPIO_MASK, STD_LOW);
-	
+
+}
+uint8_t UART0_Read (void)
+{
+ while((UART0_FR_R&0x0010)!=0);
+ return(UART0_DR_R&0xFF);
+
 }
