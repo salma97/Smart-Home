e9d6a5235088131cc0b3ea16683b1f7ed02281a5
Esraa1Moshsen <rokamohsen403@gmail.com>
2019-05-04 12:19:34 +0200

    updated #13

--- Tivac2/lcd.c
+++ Tivac2/lcd.c
@@ -79,3 +79,14 @@ void LCD_sendCommand(uint8_t command){
 					msdelay(1);
 
 }
+ void LCD_WriteChar (uint8 LCD_DATA)
+ {
+
+	 DIO_WritePort( PORTA, RS, STD_HIGH);
+	 DIO_WritePort( PORTA, RW, STD_LOW);
+	 DIO_WritePort( PORTA, E, STD_HIGH);
+	 msdelay(1)
+	 DIO_setValue( PORTB, LCD_DATA);
+	 msdelay(1)
+	 DIO_WritePort( PORTA, E, STD_LOW);
+ }
