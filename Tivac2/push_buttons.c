7453601507204caabd1799c83ad7b2168615d1f1
Esraa1Moshsen <rokamohsen403@gmail.com>
2019-05-04 12:14:41 +0200

    updated #7

--- Tivac2/push_buttons.c
+++ Tivac2/push_buttons.c
@@ -1,13 +1,20 @@
 #include "lcd.h"
 #include "push_buttons.h"
 void pushButtons_Init (void)
-{
-	void Port_Init (PF);
-	void Port_SetPinDirection (PF, 0x11, PORT_PIN_IN);
-	void Port_AnalogOrDigital (PF,0x11, 1);
-	void Port_AlternateFunction (PF, 0x11, 0, 0x00);
-	void Port_SetPinPullUp (PF, 0x11, 1);
-
+	{
+		void Port_Init (PF);
+		void Port_SetPinDirection (PF, PF_pins_mask, PORT_PIN_IN);
+		void Port_AnalogOrDigital (PF,PF_pins_mask, 1);
+		void Port_AlternateFunction (PF, PF_pins_mask, 0, PCTL_pins_mask);
+		void Port_SetPinPullUp (PF, PF_pins_mask, 1);
+		GPIO_PORTF_IS_R &= ~GPIO_PF4_0_M;
+		GPIO_PORTF_IBE_R &= ~GPIO_PF4_0_M;
+		GPIO_PORTF_IEV_R &= ~GPIO_PF4_0_M;
+		GPIO_PORTF_ICR_R |= GPIO_PF4_0_M;
+		GPIO_PORTF_IM_R |= GPIO_PF4_0_M;
+		NVIC_PRI7_R = (NVIC_PRI7_R & 0xFF00FFFF) | (GPIO_PF4_0_PRI << 21); // PRI7 bits 23:21
+		NVIC_EN0_R |= (1 << 30); // EN0 bit 30
+	}
 	
 }
 void pushButtons_ISR(void)
